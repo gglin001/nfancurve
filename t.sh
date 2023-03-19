09dad4c6170e0a7088740c6823a6732434ddb930
Allen Guo <guosonglin001@gmail.com>
2023-03-19 18:00:31 +0800

    add notes

--- t.sh
+++ t.sh
@@ -1,3 +1,4 @@
+# using lightdm launch X11 server
 systemctl enable lightdm.service
 systemctl start lightdm.service
 
@@ -7,13 +8,18 @@ systemctl start lightdm.service
 # USING bash, not work on zsh
 
 ```sh
+sudo nvidia-settings -c :0 -q all > nvidia-settings.log
+
 # seems not work
 sudo nvidia-xconfig -c :0 --cool-bits 24
 
 # sudo ./temp.sh
 
 sudo nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=1
-sudo nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=0
+# sudo nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=0
+
+# seems not work
+sudo nvidia-settings -c :0 -a [fan:0]/GPUTargetFanSpeed=10
 
 sudo nvidia-settings -c :0 -a [fan:0]/GPUTargetFanSpeed=30
 sudo nvidia-settings -c :0 -a [fan:1]/GPUTargetFanSpeed=30
