8d9ba96694305c1d9a8be957c6851995f6013580
Allen Guo <guosonglin001@gmail.com>
2023-03-19 17:49:45 +0800

    add temp scripts

---
+++ t.sh
@@ -0,0 +1,18 @@
+systemctl enable lightdm.service
+systemctl start lightdm.service
+
+# export DISPLAY=:0
+# export XAUTHORITY=/var/run/lightdm/root/:0
+
+```sh
+# seems not work
+sudo nvidia-xconfig -c :0 --cool-bits 24
+
+# sudo ./temp.sh
+
+sudo nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=1
+sudo nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=0
+
+sudo nvidia-settings -c :0 -a [fan:0]/GPUTargetFanSpeed=30
+sudo nvidia-settings -c :0 -a [fan:1]/GPUTargetFanSpeed=30
+```
